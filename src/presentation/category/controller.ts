ac3425edf72c375612c2f9643d4abb856609c3f0
Reggie Quiroz <regqc@hotmail.com>
2026-06-29 18:37:03 -0300

    feat: implement pagination functionality in category service and controller

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { CreateCategoryDto, CustomError } from "../../domain";
 import { CategoryService } from "../services/category.service";
+import { PaginationDto } from "../../domain/dtos/shared/pagination.dto";
 
 export class CategoryController {
   //DI
@@ -26,8 +27,13 @@ export class CategoryController {
   };
 
   getCategories = (req: Request, res: Response) => {
+    const { page = 1, limit = 10 } = req.query;
+
+    const [error, paginationDto] = PaginationDto.create(+page, +limit);
+    if (error) return res.status(400).json({ error });
+
     this.categoryService
-      .getCategories()
+      .getCategories(paginationDto!)
       .then((categories) => res.status(201).json(categories))
       .catch((error) => this.handleError(error, res));
   };
--- src/presentation/services/category.service.ts
+++ src/presentation/services/category.service.ts
@@ -1,5 +1,10 @@
 import { CategoryModel } from "../../data";
-import { CreateCategoryDto, CustomError, UserEntity } from "../../domain";
+import {
+  CreateCategoryDto,
+  CustomError,
+  PaginationDto,
+  UserEntity,
+} from "../../domain";
 
 export class CategoryService {
   constructor() {}
@@ -28,14 +33,33 @@ export class CategoryService {
     }
   }
 
-  async getCategories() {
+  async getCategories(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
     try {
-      const categories = await CategoryModel.find();
-      return categories.map((category) => ({
-        id: category.id,
-        name: category.name,
-        available: category.available,
-      }));
+      const [total, categories] = await Promise.all([
+        CategoryModel.countDocuments(),
+        CategoryModel.find()
+          .skip((page - 1) * limit)
+          .limit(limit),
+      ]);
+      // const categories = await CategoryModel.find()
+      //   .skip((page - 1) * limit)
+      //   .limit(limit);
+      return {
+        page,
+        limit,
+        total,
+        next: `/api/categories?page=${page + 1}&limit=${limit}`,
+        previous:
+          page - 1 > 0
+            ? `/api/categories?page=${page - 1}&limit=${limit}`
+            : null,
+        categories: categories.map((category) => ({
+          id: category.id,
+          name: category.name,
+          available: category.available,
+        })),
+      };
     } catch (error) {
       throw CustomError.internalServer("Internal server error");
     }
